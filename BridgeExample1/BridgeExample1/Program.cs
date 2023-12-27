f6568fdd928f071ff9c1bd44ba9d93d22fc6736b
Mehmet Karadana <karadanam519@gmail.com>
2023-12-27 17:58:41 +0300

    Add files via upload

---
+++ BridgeExample1/BridgeExample1/Program.cs
@@ -0,0 +1,78 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Report report = new EmployeePerformnceReport(new DesktopFormat());
+        report.display();
+
+        Console.WriteLine("*************");
+
+        Report report1 = new SalesReport(new WebFormat());
+        report1.display();
+
+    }
+}
+
+class Report
+{
+    private IReportFormat format;
+
+    public Report(IReportFormat format)
+    {
+        this.format = format;
+    }
+
+    public virtual void display()
+    {
+        format.Generate();
+    }
+}
+
+interface IReportFormat
+{
+    public void Generate();
+}
+
+class DesktopFormat() : IReportFormat
+{
+    public void Generate()
+    {
+        Console.WriteLine("rapor desktop formatında oluşturuldu");
+    }
+}
+
+class WebFormat : IReportFormat
+{
+    public void Generate()
+    {
+        Console.WriteLine("rapor web formatında oluşturuldu");
+    }
+}
+
+class SalesReport : Report
+{
+    public SalesReport(IReportFormat format) : base(format)//constructorın mirası
+    {
+    }
+
+    public override void display()
+    {
+        base.display();
+        Console.WriteLine("Satış raporu...");
+    }
+
+}
+
+class EmployeePerformnceReport : Report
+{
+    public EmployeePerformnceReport(IReportFormat format) : base(format)
+    {
+    }
+
+    public override void display()
+    {
+        base.display();
+        Console.WriteLine("employee performance raporu...");
+    }
+}
